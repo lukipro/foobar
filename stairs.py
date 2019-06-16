70543fddf20b76953139e78c88dd3685c821ab9e
Borawski, Lukasz <lukasz.borawski.ext@barco.com>
2019-06-17 00:14:05 +0200

    Seems to be working

--- stairs.py
+++ stairs.py
@@ -1,34 +1,67 @@
 #!/usr/local/bin/python2
-def stairs_ok(s):
-    prev_step_count = s[0]+1
-    for step_count in s:
-        if prev_step_count <= step_count:
-            return False
-        prev_step_count = step_count
-    return True
 
+a = []
+q = []
+k = []
 
-def create_stairs(n, stairs):
-    stairs[n] = set()
-    for t in stairs[n-1]:
-        cand = list(t)
-        for i in range(len(cand)):
-            cand[i] = cand[i]+ 1
-            if stairs_ok(cand):
-                stairs[n].add(tuple(cand))
-            cand[i] = cand[i]- 1
-        cand.append(1)
-        if stairs_ok(cand):
-            stairs[n].add(tuple(cand))
+def ak(k):
+    if k in a and k != 0:
+        return 1
+    elif -k in a and k != 0:
+        return -1
+    else:
+        return 0
+
+def qk(k):
+    print 'calling qk(' + str(k) + ')'
+    if k == 0:
+        print 'returning q[' + str(k) + '] = ' + str(1)
+        return 1
+    else:
+        qkk = ak(k)
+        print 'adding +a[' + str(k) + '] = ' + str(ak(k))
+        print 'offsets: ' + str(get_offsets(k))
+        for i in get_offsets(k):
+            if i > 0:
+                print '-q[' + str(k-i) + '] = ' + str(-q[k-i])
+                qkk = qkk - q[k-i]
+            else:
+                print '+q[' + str(k+i) + '] = ' + str(q[k+i])
+                qkk = qkk + q[k+i]
+        print 'returning q[' + str(k) + '] = ' + str(qkk)
+        return qkk
+
+def get_offsets(n):
+    offsets = []
+    for i in range(len(k)):
+        if abs(k[i]) <= n:
+            if abs(k[i]) > 0:
+                offsets.append(k[i])
+        else:
+            break
+    return offsets
 
 def solution(n):
-    stairs = {3: {tuple([2,1])}}
-    for i in range(3, n):
-        create_stairs(i+1, stairs)
+    for m in range(n):
+        k.append((3*m*m-m)/2*(-1)**m)
+        k.append((3*m*m+m)/2*(-1)**m)
+    
+    print 'k: ' + str(k) 
+    
+    for m in range(n):
+        a.append((3*m*m-m)*(-1)**m)
+        a.append((3*m*m+m)*(-1)**m)
+    
+    print 'a: ' + str(a) 
 
-    #print stairs[n]
-    return len(stairs[n])
+    for i in range(n):
+        print 'a(' + str(i) + '): ' + str(ak(i))
 
-#print solution(200)
-for n in range(4,100):
-     print 'n: ' + str(n) + ' res: ' + str(solution(n))
+
+    for i in range(n+1):
+        q.append(qk(i))
+
+    print q
+
+
+solution(200)
